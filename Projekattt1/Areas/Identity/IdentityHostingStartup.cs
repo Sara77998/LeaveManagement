a943dd3850c74d38112f740a4bf55b5147df819f
Sara <saracanovic@gmail.com>
2021-10-12 16:22:15 +0200

    Dodavanje usera

---
+++ Projekattt1/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Projekattt1.Data;
+
+[assembly: HostingStartup(typeof(Projekattt1.Areas.Identity.IdentityHostingStartup))]
+namespace Projekattt1.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
