caabcc957ab712bb5e050b123af53e5bcfdd96f3
Sara <saracanovic@gmail.com>
2021-10-22 12:02:15 +0200

    Sitne izmene

--- Projekattt1/Models/EmployeeVM.cs
+++ Projekattt1/Models/EmployeeVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,14 +9,21 @@ namespace Projekattt1.Models
     public class EmployeeVM
     {
         public string Id { get; set; }
+        [Display(Name = "User")]
         public string UserName { get; set; }//mora da se mecuje sa tipom iz baze
+    
         public string Email { get; set; }
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
-
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Display(Name = "Tax ID Number")]
         public string TaxtId { get; set; }
+        [Display(Name = "Date Of Birth")]
         public DateTime DateOfBirth { get; set; }
+        [Display(Name = "Date Joined")]
         public DateTime DateJoined { get; set; }
     }
 }
